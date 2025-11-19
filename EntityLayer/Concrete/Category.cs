7c1813bef475a173434a4d56fb5bd46209bdea5e
metndurn <mtndrn123@gmail.com>
2025-11-19 18:44:14 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+	public class Category
+	{
+		public int Id { get; set; }
+		public string CategryName { get; set; }
+		public string CategoryDescription { get; set; }
+		public string CategoryStatus { get; set; }
+	}
+}
---
+++ EntityLayer/Concrete/Content.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+	public class Content
+	{
+		public int Id { get; set; }
+		public string ContentValue { get; set; }
+		public DateTime ContentDate { get; set; }
+		//ContentYazar ilişkili olacagı ıcın bunlar olacak yanı bu yazı kım tarafından yazıldıgını belırtecegız
+		//ContentBaslik ılıskısı ıcınde bunlar olacak yanı bu yazı hangi baslıga aıt oldugunu belırtecegız
+	}
+}
