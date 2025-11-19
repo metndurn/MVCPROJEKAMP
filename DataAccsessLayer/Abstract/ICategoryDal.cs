977448806a72c3741185e152a7a1124bec4d8ee9
metndurn <mtndrn123@gmail.com>
2025-11-20 00:18:13 +0300

    Mvc Proje Kampı 14 Abstract Ve Interface Bileşenleri

---
+++ DataAccsessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccsessLayer.Abstract
+{
+	public interface ICategoryDal
+	{
+		//CRUD OPERASYONLARI İÇİN METOTLAR
+		List<Category> List();
+		void Insert(Category category);
+		void Update(Category category);
+		void Delete(Category category);
+	}
+}
