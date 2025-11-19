c8d43bd8541140f66a367a27e6c4152b774c19c4
metndurn <mtndrn123@gmail.com>
2025-11-19 22:24:56 +0300

    Mvc Proje Kampı 8 Key Ve String Length Attributeleri eklendi

--- EntityLayer/Concrete/About.cs
+++ EntityLayer/Concrete/About.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,19 @@ namespace EntityLayer.Concrete
 {
 	public class About
 	{
+		[Key]// Primary Key olacak demek
 		public int AboutId { get; set; }
+
+		[StringLength(1000)]// Maksimum karakter sayısı
 		public string AboutDetails1 { get; set; }
+
+		[StringLength(1000)]
 		public string AboutDetails2 { get; set; }
+
+		[StringLength(100)]
 		public string AboutImage1 { get; set; }
+
+		[StringLength(100)]
 		public string AboutImage2 { get; set; }
 	}
 }
--- EntityLayer/Concrete/Writer.cs
+++ EntityLayer/Concrete/Writer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,22 @@ namespace EntityLayer.Concrete
 {
 	public class Writer
 	{
+		[Key]
 		public int WriterId { get; set; }
-		public string WtierName { get; set; }
+
+		[StringLength(50)]
+		public string WriterName { get; set; }
+
+		[StringLength(50)]
 		public string WriterSurname { get; set; }
+
+		[StringLength(100)]
 		public string WriterImage { get; set; }
+
+		[StringLength(30)]
 		public string WriterMail { get; set; }
+
+		[StringLength(20)]
 		public string WriterPassword { get; set; }
 
 		public ICollection<Heading> Headings { get; set; }//bir yazarın birden fazla baslıgı olabilir demektir
