cbd996875163cf505ae670de0a6c8a99a66e7dd2
metndurn <mtndrn123@gmail.com>
2025-11-20 01:04:45 +0300

    Mvc Proje Kampı 18 Generic Repository

---
+++ DataAccsessLayer/Concrete/Repositories/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using DataAccsessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccsessLayer.Concrete.Repositories
+{
+	public class GenericRepository<T> : IRepository<T> where T : class
+	{
+		Context Context = new Context();
+		DbSet<T> _object;
+		public GenericRepository()//constructor metodu 
+		{
+			_object = Context.Set<T>();//hangi tipte bir nesne gelirse gelsin contextden o nesneye ait dbseti alır
+		}
+
+		public void Delete(T t)
+		{
+			_object.Remove(t);
+			Context.SaveChanges();
+		}
+
+		public void Insert(T t)
+		{
+			_object.Add(t);
+			Context.SaveChanges();
+		}
+
+		public List<T> List()
+		{
+			return _object.ToList();
+		}
+
+		public List<T> List(Expression<Func<T, bool>> filter)
+		{
+			return _object.Where(filter).ToList();
+		}
+
+		public void Update(T t)
+		{
+			Context.SaveChanges();
+		}
+	}
+}
