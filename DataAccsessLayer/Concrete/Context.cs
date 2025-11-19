3251f141a887323f6cca3008bbb0f75af0a785da
metndurn <mtndrn123@gmail.com>
2025-11-19 22:33:49 +0300

    Mvc Proje Kampı 9 Context Sınıfı olusturuldu

---
+++ DataAccsessLayer/Concrete/Context.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccsessLayer.Concrete
+{
+	public class Context : DbContext //entity frameworkun dbcontext sınıfına ihtiyacımız var yazdıgımız dbsetler entitylayerdaki classlarla baglantı kuracak ve veritabanında tabloları olusturacak
+	{
+		public DbSet<About> Abouts { get; set; }
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<Contact> Contacts { get; set; }
+		public DbSet<Content> Contents { get; set; }
+		public DbSet<Heading> Headings { get; set; }
+		public DbSet<Writer> Writers { get; set; }
+	}
+}
