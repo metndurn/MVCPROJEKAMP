5b48ab80da512082491ded69f65e80a029646ea2
metndurn <mtndrn123@gmail.com>
2025-11-20 20:03:34 +0300

    Mvc Proje Kampı 19 Business Layer

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,31 @@
+﻿using DataAccsessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+	public class CategoryManager
+	{
+		GenericRepository<Category> repoCategory = new GenericRepository<Category>();//burada category repositoryden bir nesne türettik
+
+		//her bır crud ıslemı ıcın metodlar yazıyoruz
+		public List<Category> GetAllBL()//tüm kategorileri listeleme metodu
+		{
+			return repoCategory.List();
+		}
+		public void CategoryAddBL(Category category)//kategori ekleme metodu
+		{
+			/*burada ki kosulda herhangı bır hata gelırse kod hatası olarak eklenme me uyarısı verılecek*/
+			if (category.CategoryName == "" || category.CategoryName.Length <= 3 ||
+				category.CategoryDescription == "" || category.CategoryName.Length > 51)
+			{
+
+			}
+		}
+
+	}
+}
