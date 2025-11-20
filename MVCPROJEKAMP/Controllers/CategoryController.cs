b2566cdb3147175fe8e490437fb5874311310bba
metndurn <mtndrn123@gmail.com>
2025-11-20 22:30:01 +0300

    Mvc Proje Kampı 22 Kategorilerin Listelenmesi

--- MVCPROJEKAMP/Controllers/CategoryController.cs
+++ MVCPROJEKAMP/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,15 @@ namespace MVCPROJEKAMP.Controllers
     public class CategoryController : Controller
     {
         // GET: Category
-        public ActionResult Index()
+        CategoryManager CategoryManager = new CategoryManager();
+		public ActionResult Index()
         {
             return View();
         }
-    }
+		public ActionResult GetCategoryList()//kategori listeleme işlemi
+		{
+			var categoryvalues = CategoryManager.GetAllBL();//tüm kategorileri getir
+			return View(categoryvalues);
+		}
+	}
 }
